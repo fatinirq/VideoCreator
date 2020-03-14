f1a068c1086f2e897b19fee71c12834af1398fba
fatinirq <fatinirq@gmail.com>
2020-03-14 23:00:29 +0300

    Add files via upload
    second commit

---
+++ AVIW/AVIW/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports AForge.Video.VFW
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim writer As AVIWriter = New AVIWriter
+        Dim bmp As Bitmap = New Bitmap("Frame1.bmp")
+        Dim j As Integer = 0
+        Dim r As Random = New Random
+
+
+        writer.Open("NewVideo2.avi", bmp.Width, bmp.Height)
+        For i = 0 To 60
+
+            Dim image As Bitmap = New Bitmap("F0.bmp")
+
+            writer.AddFrame(image)
+        Next
+        For i = 0 To 60
+
+            Dim image As Bitmap = New Bitmap("Frame" + j.ToString + ".bmp")
+            j = r.Next(0, 9)
+            writer.AddFrame(image)
+            writer.AddFrame(image)
+            writer.AddFrame(image)
+        Next
+        writer.Close()
+
+    End Sub
+End Class
